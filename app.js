bc96df44ead5f35334da2ca21a03cfb1719da186
danijeladukic <danijeladolic7@gmail.com>
2023-02-12 02:08:44 +0100

    2nd commit: added directions for coding

---
+++ app.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    //initialize constants and variables
+
+
+    //setInterval() that calles moveOutcomes
+
+    //moveOutcomes: 1)empty spot 2)eats apple 3)game ended: hits walls(4 options)/eats itself
+
+
+    //on click of a button game starts
+
+    //document event listener on key up->checks if direction changed(up/down/left/right)
+
+    //function random apple
+
+    //animation when game over
+
+
+
+})
